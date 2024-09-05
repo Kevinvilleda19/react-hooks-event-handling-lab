9a02af226903aa69086ea0bfc2a1374fb735e7da
Kevin <kvilleda10566@gmail.com>
2024-09-05 12:55:59 -0400

    Done

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,26 @@
 // Code EyesOnMe Component Here
+import React from "react";
+
+
+function EyesOnMe () {
+
+   function handlefocus() {
+    console.log('Good!');
+   }
+   function handleblur() {
+    console.log('Hey! Eyes on me!');
+   }
+
+    return (
+        <div>
+            <button
+            onFocus={handlefocus}
+            onBlur={handleblur}
+            >
+                Eyes on me
+            </button>
+        </div>
+    );
+}
+
+export default EyesOnMe;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,9 +1,38 @@
 // Code Keypad Component Here
+import React from "react";
 
-function Keypad (){
+function Keypad({ onClick }) {
+    const keys = [
+      '1', '2', '3',
+      '4', '5', '6',
+      '7', '8', '9',
+      '*', '0', '#'
+    ];
+
+    function handleInputChange() {
+      console.log('Entering password...');
+    }
+
+    function handleButtonClick(key) {
+      onClick(key);
+    }
+
     return (
-        <div></div>
-    )
-}
+      <div className="keypad">
+        <input
+          type="password"
+          placeholder="Enter your password"
+          onChange={handleInputChange}
+        />
+        {keys.map(function(key) {
+          return (
+            <button key={key} onClick={function() { handleButtonClick(key); }}>
+              {key}
+            </button>
+          );
+        })}
+      </div>
+    );
+  }
 
-export default Keypad;
+  export default Keypad;
